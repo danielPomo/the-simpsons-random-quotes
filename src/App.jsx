4b30848cd599e9ce2ffc7458210a1f10be9b9b1b
danielPomo <danielnicolaspomo@gmail.com>
2023-06-27 00:33:46 -0300

    I style the spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
-import { Button, Container } from "react-bootstrap";
+import { Button, Container} from "react-bootstrap";
 import logo from "./assets/logo.png";
+import spinner from './assets/spinner.png'
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "./components/Card";
@@ -8,6 +9,7 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [quote, setQuote] = useState({});
+  const [showSpinner, setShowSpinner] = useState(true);
 
   useEffect(() => {
     getQuoteFromAPI();
@@ -15,15 +17,25 @@ function App() {
 
   const getQuoteFromAPI = async () => {
     try {
+      setShowSpinner(true);
       const res = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
       const data = await res.json();
       console.log(res);
       console.log(data[0]);
       setQuote(data[0]);
+      setShowSpinner(false);
     } catch (error) {
       console.log(error);
     }
   };
+
+  const activeComponent = showSpinner ? (
+    <div className="spinner d-flex justify-content-center align-items-center">
+      <img src={spinner} alt="spinner" className="spinner-img w-50" />
+    </div>
+  ) : (
+    <Card quote={quote} />
+  );
   return (
     <>
       <div className="flex-center wrapper">
@@ -38,8 +50,12 @@ function App() {
             <hr />
           </section>
         </Container>
-        <Card quote={quote} />
-        <Button variant="dark" className="w-50 text-center mt-2 mb-5 quote-btn" onClick={getQuoteFromAPI}>
+        {activeComponent}
+        <Button
+          variant="dark"
+          className="w-50 text-center mt-2 mb-5 quote-btn"
+          onClick={getQuoteFromAPI}
+        >
           Try again!
         </Button>
       </div>
